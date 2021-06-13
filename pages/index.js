d705d9abf654e5aab6daa0220ca1529f577262d3
shubham-1903 <shubhamkr7677@gmail.com>
2021-06-13 14:00:33 +0530

    modified files

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({data}) {
+  const recipes=data.recipe; 
   return (
     <div className={styles.container}>
       <Head>
@@ -13,7 +14,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a>{recipes[0].title}</a>
         </h1>
 
         <p className={styles.description}>
@@ -66,4 +67,14 @@ export default function Home() {
       </footer>
     </div>
   )
+}
+
+export function getStaticProps() {
+  return {
+    props: {
+      data: {
+        recipe: [{title: "pineapple"}],
+      },
+    },
+  }
 }
